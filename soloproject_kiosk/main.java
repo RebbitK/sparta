555924460a888f89ed055c3d5159a082a18c4b76
RebbitK <154823447+RebbitK@users.noreply.github.com>
2024-01-04 15:59:39 +0900

    Delete soloproject_kiosk directory

--- soloproject_kiosk/main.java
+++
@@ -1,9 +0,0 @@
-package soloproject_kiosk;
-
-public class main {
-    public static void main(String[] args){
-        //screen 객체를 생성해 main_screen()메서드를 실행시킵니다.
-        screen s = new screen();
-        s.main_screen();
-    }
-}
